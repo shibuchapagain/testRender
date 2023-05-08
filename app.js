9eff0e867413bb42d9b480148c5187b486f07a16
shiva chapagain <shibuchapagain12@gmail.com>
2023-05-08 12:15:44 +0545

    update

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Servers listening on port ${port}.`);
+  console.log(`Serverss listening on port ${port}.`);
 });
