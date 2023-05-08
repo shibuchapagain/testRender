e3d85e5cfd440556d56258e90ba0cea5de125dfc
shiva chapagain <shibuchapagain12@gmail.com>
2023-05-08 11:59:55 +0545

    last update

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
-
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("HI IAM IN LIVE");
+  res.send("Hello, world!");
 });
 
-app.listen(3000, () => {
-  console.log("server at 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
 });
